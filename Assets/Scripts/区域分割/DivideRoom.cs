4bb924a07d863c65f32c054e6caa672a567b34bc
TsuyoshiUsugi <qesulive@gmail.com>
2023-03-30 21:27:44 +0900

    [add] 穴掘り法のスクリプトを追加

--- Assets/Scripts/区域分割/DivideRoom.cs
+++ Assets/Scripts/区域分割/DivideRoom.cs
@@ -25,12 +25,14 @@ public class DivideRoom : MonoBehaviour
     //一つのエリアの横幅の大きさ
     int _areaSize = 1;
 
+    //出来たエリアの中心座標X
     int _randomPosWidth;
 
+    //出来たエリアの中心座標Y
     int _randomPosHeight;
 
     int _roomSize;
-    
+
     int _keepBackSide;　//生成した部屋の一番最後にあるオブジェクトのｘ座標を管理する変数？
     int _keepFrontSide;　//生成した部屋の一番最初にあるオブジェクトのｘ座標を管理する変数？
     int _keepPosHeight; //生成した部屋の一番最初にあるオブジェクトのｘ座標を管理する変数？
@@ -50,23 +52,23 @@ public class DivideRoom : MonoBehaviour
         //ここからエリアの生成を始める
         for (int i = 0; i < _areaNum; i++)
         {
-            preAreaStartPoint = currentAreaStartPoint; 
+            preAreaStartPoint = currentAreaStartPoint;
 
             //最初の区画の場合
-            if(i == 0)
+            if (i == 0)
             {
                 currentAreaStartPoint = _areaSize;
                 //横幅の中から部屋を作る時に中心となる座標のX座標を割り出す
-                _randomPosWidth = Random.Range(1, currentAreaStartPoint); 
+                _randomPosWidth = Random.Range(1, currentAreaStartPoint);
             }
             //最後の区画の場合
-            else if(i == _areaNum - 1)
+            else if (i == _areaNum - 1)
             {
                 //最後の区画だからこうなるのは当然
                 currentAreaStartPoint = _mapWidth - 1;
                 Debug.Log(preAreaStartPoint + "前回の最大の幅");
                 Debug.Log(currentAreaStartPoint + "今回の最大の幅");
-                _randomPosWidth = Random.Range(preAreaStartPoint, currentAreaStartPoint); 
+                _randomPosWidth = Random.Range(preAreaStartPoint, currentAreaStartPoint);
             }
             //間の区画の場合
             else
@@ -74,7 +76,7 @@ public class DivideRoom : MonoBehaviour
                 currentAreaStartPoint += _areaSize;
                 Debug.Log(preAreaStartPoint + "前回の最大の幅");
                 Debug.Log(currentAreaStartPoint + "今回の最大の幅");
-                _randomPosWidth = Random.Range(preAreaStartPoint, currentAreaStartPoint); 
+                _randomPosWidth = Random.Range(preAreaStartPoint, currentAreaStartPoint);
             }
 
             _randomPosHeight = Random.Range(1, _mapHeight);
@@ -84,69 +86,74 @@ public class DivideRoom : MonoBehaviour
 
             for (int x = _randomPosWidth - _roomSize; x < _randomPosWidth + _roomSize; x++)　//ここから部屋の生成。しかしこのままでははみ出ないか？
             {
-                Debug.Log(1);
                 for (int y = _randomPosHeight - _roomSize; y < _randomPosHeight + _roomSize; y++)
                 {
-                    Debug.Log(2);
                     if (x > 0 && x < currentAreaStartPoint)　//ここで制限は出来ている
                     {
-                        Debug.Log(3);
                         if (x > preAreaStartPoint)
                         {
-                            Debug.Log(4);
                             if (y > 0 && y < _mapHeight)
                             {
                                 var tile = Instantiate(_tile);
                                 tile.transform.position = new Vector3(x, y, 0);
 
-                                _keepBackSide = x;　//謎の処理
-                                _count++;
+                                //ここから部屋をつなげる処理
+                                //つなげ方は前の部屋の最後のX座標のタイルと今回作った部屋の
+                                //一番最初に生成した横の座標をつなげるようにオブジェクトを
+                                //生成する
+
+                                //ここは一番最後のX座標を保持している
+                                _keepBackSide = x;
+
+                                _count++;　
                             }
-                            
-                            
                         }
                     }
 
-                    if (_count == 1) //謎の処理
+                    //ここでカウントが１なら一番最初のX座標を保持する
+                    if (_count == 1)
                     {
                         _keepFrontSide = x;
                     }
 
-                    if (i != _areaNum - 1)
-                    {
-                        for (int road = _keepBackSide + 1; road < currentAreaStartPoint; road++)
-                        {
-                            var tile = Instantiate(_tile);
-                            tile.transform.position = new Vector3(road, _randomPosHeight, 0);
-                        }
-                    }
+                }
+            }
+            _count = 0;
 
-                    if (i > 0)
-                    {
-                        for (int aisle = _keepFrontSide; aisle >= preAreaStartPoint; aisle--)
-                        {
-                            var tile = Instantiate(_tile);
-                            tile.transform.position = new Vector3(aisle, _randomPosHeight, 0);
-                        }
-                    }
+            
+            if (i != _areaNum - 1)　//生成したエリアの最後尾のマスから今回の最大幅まで道をつなげる？
+            {
+                for (int road = _keepBackSide + 1; road <= currentAreaStartPoint; road++)
+                {
+                    var tile = Instantiate(_tile);
+                    tile.transform.position = new Vector3(road, _randomPosHeight, 0);
+                }
+            }
 
-                    if (_randomPosHeight > _keepPosHeight)
+            if (i > 0)
+            {
+                for (int aisle = _keepFrontSide; aisle >= preAreaStartPoint; aisle--)
+                {
+                    var tile = Instantiate(_tile);
+                    tile.transform.position = new Vector3(aisle, _randomPosHeight, 0);
+                }
+
+                if (_randomPosHeight > _keepPosHeight)
+                {
+                    //道をつなげるコード？
+                    for (int road = _keepPosHeight; road < _randomPosHeight; road++)
                     {
-                        //道をつなげるコード？
-                        for (int road = _keepPosHeight; road < _randomPosHeight; road++)
-                        {
-                            var tile = Instantiate(_tile);
-                            tile.transform.position = new Vector3(preAreaStartPoint, road, 0);
-                        }
+                        var tile = Instantiate(_tile);
+                        tile.transform.position = new Vector3(preAreaStartPoint, road, 0);
                     }
-                    else
+                }
+                else
+                {
+                    //道をつなげるためのコード？
+                    for (int road = _randomPosHeight + 1; road < _randomPosHeight; road++)
                     {
-                        //道をつなげるためのコード？
-                        for (int road = _randomPosHeight + 1; road < _randomPosHeight; road++)
-                        {
-                            var tile = Instantiate(_tile);
-                            tile.transform.position = new Vector3(preAreaStartPoint, road, 0);
-                        }
+                        var tile = Instantiate(_tile);
+                        tile.transform.position = new Vector3(preAreaStartPoint, road, 0);
                     }
                 }
             }
